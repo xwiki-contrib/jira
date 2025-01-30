c460011b8b483c5a8a82b33a2cc5cd6eafd2e58e
Teodor Caras <trrenty@gmail.com>
2025-01-30 10:22:16 +0200

    JIRA-65: Provide a ckeditor plugin that replaces pasted jira links with a jira macro
    * Additionally test that the ckeditor paste renders correctly.

--- jira-test/jira-test-docker/src/test/it/org/xwiki/contrib/jira/test/ui/PastePluginIT.java
+++ jira-test/jira-test-docker/src/test/it/org/xwiki/contrib/jira/test/ui/PastePluginIT.java
@@ -19,6 +19,7 @@
  */
 package org.xwiki.contrib.jira.test.ui;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -35,6 +36,11 @@ import org.xwiki.test.ui.po.ViewPage;
 import org.xwiki.test.ui.po.editor.WYSIWYGEditPage;
 import org.xwiki.test.ui.po.editor.WikiEditPage;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -48,18 +54,37 @@ public class PastePluginIT
 {
     private static final String ADMIN_CONFIG_JIRA_ID = "testId";
 
-    private static final String ADMIN_CONFIG_JIRA_URL = "https://localhost:8889";
+    private static final String ADMIN_CONFIG_JIRA_URL = "http://localhost:8889";
 
     private static final EntityReference TEST_PAGE_REFERENCE = new DocumentReference("wiki", "Test", "Macro");
 
     private static final String LINK_JIRA_TICKET = ADMIN_CONFIG_JIRA_URL + "/browse/XWIKI-1000";
 
-    private static final String LINK_JIRA_JQL = ADMIN_CONFIG_JIRA_URL + "/browse/JIRA-65?jql=project%20%3D%20JIRA"
-        + "%20AND%20resolution%20%3D%20Unresolved%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC";
+    private static final String LINK_JIRA_JQL =
+        ADMIN_CONFIG_JIRA_URL + "/issues/?jql=issuekey%20in%20(XWIKI-1000%2C%20XWIKI-1001)";
+
+    private WireMockServer wireMockServer;
+
+    @AfterAll
+    void noMoreWireMock()
+    {
+        this.wireMockServer.stop();
+        this.wireMockServer = null;
+    }
 
     @BeforeAll
     void setup(TestUtils setup)
     {
+        this.wireMockServer = new WireMockServer(8889);
+        this.wireMockServer.start();
+
+        this.wireMockServer.stubFor(get(urlMatching(
+            "\\/sr\\/jira.issueviews:searchrequest-xml\\/temp\\/SearchRequest\\.xml\\?jqlQuery=.*"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "text/xml")
+                .withBodyFile("input.xml")));
+
         setup.deletePage(TEST_PAGE_REFERENCE);
 
         setup.loginAsSuperAdmin();
@@ -90,9 +115,8 @@ public class PastePluginIT
         copyInClipboard(linkFromUnconfiguredJiraInstance, setup);
 
         ViewPage vp = setup.gotoPage(TEST_PAGE_REFERENCE);
-        WikiEditPage wikiEditPage = pasteClipboardInCKEditorAndEditWiki(vp, setup, true);
 
-        String content = wikiEditPage.getContent();
+        String content = pasteClipboardInCKEditorAndEditWiki(vp, setup, true);
         assertEquals(
             "[[https:~~/~~/jira.unconfigured.test/browse/TICKET-134>>https://jira.unconfigured.test/browse/TICKET-134]]",
             content);
@@ -104,10 +128,9 @@ public class PastePluginIT
         copyInClipboard(LINK_JIRA_TICKET, setup);
 
         ViewPage vp = setup.gotoPage(TEST_PAGE_REFERENCE);
-        WikiEditPage wikiEditPage = pasteClipboardInCKEditorAndEditWiki(vp, setup, true);
 
-        String content = wikiEditPage.getContent();
-        assertEquals("[[https:~~/~~/localhost:8889/browse/XWIKI-1000>>https://localhost:8889/browse/XWIKI-1000]]",
+        String content = pasteClipboardInCKEditorAndEditWiki(vp, setup, true);
+        assertEquals("[[http:~~/~~/localhost:8889/browse/XWIKI-1000>>http://localhost:8889/browse/XWIKI-1000]]",
             content);
     }
 
@@ -117,9 +140,16 @@ public class PastePluginIT
         copyInClipboard(LINK_JIRA_TICKET, setup);
 
         ViewPage vp = setup.gotoPage(TEST_PAGE_REFERENCE);
-        WikiEditPage wikiEditPage = pasteClipboardInCKEditorAndEditWiki(vp, setup, false);
+        WYSIWYGEditPage editPage = vp.editWYSIWYG();
+        CKEditor ckEditor = new CKEditor("content").waitToLoad();
+        setup.getDriver().findElement(By.className("cke_notification_close")).click();
+        String paste = Keys.chord(Keys.CONTROL, "v");
+        ckEditor.getRichTextArea().sendKeys(paste);
 
-        String content = wikiEditPage.getContent();
+        setup.getDriver().waitUntilCondition(
+            webDriver -> ckEditor.getRichTextArea().getText().equals("XWIKI-1000 Improve PDF Output"));
+
+        String content = saveAndGetContent(editPage);
         assertEquals(
             "{{jira id=\"testId\" url=\"\" style=\"enum\" fields=\"type,key,summary,status\" source=\"list\" maxCount=\"20\"}}\n"
                 + "XWIKI-1000\n"
@@ -132,16 +162,33 @@ public class PastePluginIT
         copyInClipboard(LINK_JIRA_JQL, setup);
 
         ViewPage vp = setup.gotoPage(TEST_PAGE_REFERENCE);
-        WikiEditPage wikiEditPage = pasteClipboardInCKEditorAndEditWiki(vp, setup, false);
+        WYSIWYGEditPage editPage = vp.editWYSIWYG();
+        CKEditor ckEditor = new CKEditor("content").waitToLoad();
+        setup.getDriver().findElement(By.className("cke_notification_close")).click();
+        String paste = Keys.chord(Keys.CONTROL, "v");
+        ckEditor.getRichTextArea().sendKeys(paste);
 
-        String content = wikiEditPage.getContent();
+        setup.getDriver()
+            .waitUntilCondition(webDriver -> ckEditor.getRichTextArea().getText().equals("Type Key Summary Status\n"
+                + "XWIKI-1001 On jetty, non-default skins are not usable\n"
+                + "XWIKI-1000 Improve PDF Output"));
+
+        String content = saveAndGetContent(editPage);
         assertEquals(
             "{{jira id=\"testId\" url=\"\" style=\"table\" fields=\"type,key,summary,status\" source=\"jql\" maxCount=\"20\"}}\n"
-                + "project = JIRA AND resolution = Unresolved ORDER BY priority DESC, updated DESC\n"
+                + "issuekey in (XWIKI-1000, XWIKI-1001)\n"
                 + "{{/jira}}", content);
     }
 
-    private WikiEditPage pasteClipboardInCKEditorAndEditWiki(ViewPage page, TestUtils setup, boolean withoutFormatting)
+    private String saveAndGetContent(WYSIWYGEditPage editPage)
+    {
+        editPage.clickSaveAndContinue(true);
+        WikiEditPage wikiEditPage = editPage.editWiki();
+        wikiEditPage.waitUntilPageIsReady();
+        return wikiEditPage.getContent();
+    }
+
+    private String pasteClipboardInCKEditorAndEditWiki(ViewPage page, TestUtils setup, boolean withoutFormatting)
     {
         WYSIWYGEditPage editPage = page.editWYSIWYG();
         CKEditor ckEditor = new CKEditor("content").waitToLoad();
@@ -150,12 +197,8 @@ public class PastePluginIT
         String paste =
             withoutFormatting ? Keys.chord(Keys.CONTROL, Keys.LEFT_SHIFT, "v") : Keys.chord(Keys.CONTROL, "v");
         ckEditor.getRichTextArea().sendKeys(paste);
-        editPage.clickSaveAndContinue(true);
 
-        WikiEditPage wikiEditPage = editPage.editWiki();
-        wikiEditPage.waitUntilPageIsReady();
-
-        return wikiEditPage;
+        return saveAndGetContent(editPage);
     }
 
     private void copyInClipboard(String stringToCopy, TestUtils setup)
