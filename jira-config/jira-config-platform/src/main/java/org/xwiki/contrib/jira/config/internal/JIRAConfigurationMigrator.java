ddbd8d6883bf24099d5502952f7f22611b58c3e4
Josué Tille <josue.tille@xwiki.com>
2025-11-16 17:41:01 +0100

    JIRA-99: Owner of Basic auth configuration page should be allways superadmin
    * Fix comment

--- jira-config/jira-config-platform/src/main/java/org/xwiki/contrib/jira/config/internal/JIRAConfigurationMigrator.java
+++ jira-config/jira-config-platform/src/main/java/org/xwiki/contrib/jira/config/internal/JIRAConfigurationMigrator.java
@@ -161,7 +161,7 @@ public class JIRAConfigurationMigrator extends AbstractEventListener
                 authors.setEffectiveMetadataAuthor(SuperAdminUserReference.INSTANCE);
                 authors.setOriginalMetadataAuthor(SuperAdminUserReference.INSTANCE);
 
-                xwiki.saveDocument(doc, "Set superuser as creator and author", true, context);
+                xwiki.saveDocument(doc, "Set superadmin as creator and author", true, context);
             }
         }
     }
